5d164558dc18fb7da53d5aef3f31f6f32b391b1e
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-02-18 10:25:37 +0100

    Update on snapshot and backup recipes

--- bsc-erigon/compress-snapshot.sh
+++ bsc-erigon/compress-snapshot.sh
@@ -2,14 +2,19 @@
 #
 # Create an Erigon snapshot archive.
 # Make sure the Erigon docker is stopped before running this.
+# We compress chaindata, bor and nodes folder of Erigon data.
 #
+
+# To begin
+# apt install pv zstd
 
 set -e
 
 # zstd archives have .zst extension
+DATA_DIR=/bsc/data/bsc-erigon/
 TARGET=/bsc/snapshot-2023-02.tar.zst
 
-time (cd /bsc/data/bsc-erigon/ && tar -cf $TARGET --use-compress-program=zstdmt chaindata bor nodes)
+time (cd $DATA_DIR && tar -cf - chaindata bor nodes parlia | pv | zstd -f -T0 -8 -o $TARGET)
 
 # Viewing archive contents
 # tar -tf $TARGET --use-compress-program=zstdmt
---
+++ bsc-erigon/upload-snapshot.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Upload snapshot to Storj cloud
+#
+#
+# To install Uplink CLI, see https://docs.storj.io/dcs/downloads/download-uplink-cli
+#
+#   curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
+#   unzip -o uplink_linux_amd64.zip
+#   sudo install uplink /usr/local/bin/uplink
+#
+# Then you need to get Access grant in Storj web UI, it's 1024 bytes string
+#
+#   export ACCESS_GRANT=1Hy5s...
+#   uplink access import main $ACCESS_GRANT
+#
+# This is an interactive (y/n) process and will create /root/.config/storj/uplink/access.json
+#
+#
+set -e
+
+# The snapshot archive we are uploading
+TARGET=/bsc/snapshot-2023-02.tar.zst
+
+# Created in the web UI
+BUCKET=bnb-chain-snapshot
+
+# Upload rate 20 MBytes/sec
+uplink cp $TARGET sj://$BUCKET
